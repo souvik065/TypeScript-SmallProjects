d7cf1fa14bfae8ef06e5e36000e45afcdf127a12
souvik3950 <bhattacharyasouvik39@gmail.com>
2023-08-02 22:56:20 +0530

    Bundler TypeScript Setup and Initiate the Build of Todo List App

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {v4 as uuidV4} from "uuid"
 
-import confetti from 'canvas-confetti';
+console.log(uuidV4);
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>('#list');
+const form  = document.querySelector<HTMLFormElement>('#new-task-form');
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+
+form?.addEventListener("submit",e=>{
+  e.preventDefault()
+
+  if(input?.value == ""||input?.value==null)return
+
+  const task={
+    id:uuidV4(),
+    title:input.value,
+    compeleted:false,
+    createdAt:new  Date()
+  }
+})
