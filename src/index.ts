7b70c1882219e32dd98b2523c64880d63e75d677
souvik3950 <bhattacharyasouvik39@gmail.com>
2023-08-03 12:42:17 +0530

    Todo List Compeleted

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
+import { addListener } from "process";
 import {v4 as uuidV4} from "uuid"
+
+type Task={
+  id:string
+  title:string
+  completed:boolean
+  createdAt:Date
+}
 
 console.log(uuidV4);
 
@@ -6,15 +14,53 @@ const list = document.querySelector<HTMLUListElement>('#list');
 const form  = document.querySelector<HTMLFormElement>('#new-task-form');
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
 
+const tasks:Task[] = loadTasks()
+tasks.forEach(addListItem)
+
 form?.addEventListener("submit",e=>{
   e.preventDefault()
 
   if(input?.value == ""||input?.value==null)return
 
-  const task={
+  const newTask:Task={
     id:uuidV4(),
     title:input.value,
-    compeleted:false,
+    completed:false,
     createdAt:new  Date()
   }
+  tasks.push(newTask)
+  saveTasks()
+
+  addListItem(newTask)
+  input.value = ""
 })
+
+
+function addListItem(task:Task){
+  const item = document.createElement("li") 
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+
+  checkbox.addEventListener("change",()=>{
+    task.completed = checkbox.checked
+    saveTasks()
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+  label.append(checkbox,task.title)
+  item.append(label)
+  list?.append(item)
+
+}
+
+function saveTasks(){
+  localStorage.setItem("TASKS",JSON.stringify(tasks))
+}
+
+function loadTasks():Task[]{
+  console.log("Loading Task")
+  const taskJSON = localStorage.getItem("TASKS")
+  if(taskJSON == null) return []
+  console.log(taskJSON)
+  return JSON.parse(taskJSON)
+}
